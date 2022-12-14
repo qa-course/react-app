59b5a5f0eceeb3168322d2383f0cb196897fc2aa
Aleksei Domozhirov <aldomozhirov@gmail.com>
2022-12-14 18:09:34 +0200

    Изменена ссылка на репозиторий

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://github.com/qa-course/react-app"
+          href="https://github.com/qa-course/react-app/tree/develop"
           target="_blank"
           rel="noopener noreferrer"
         >
